02efcaa4a7388e68e9e2b149baf1e5af306e07e1
Chris <corez92@users.noreply.github.com>
2016-08-17 13:53:36 -0400

    UI basics added, token generator added and views added

--- tele_giphy/game/urls.py
+++ tele_giphy/game/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<token>[0-9]+)/$', views.wait, name='wait'),
 ]
--- tele_giphy/game/views.py
+++ tele_giphy/game/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'game/index.html')
+	token = ""
+	for i in range(4):
+		token = token + random.choice('1234567890')
+	return render(request, 'game/index.html', {"token": token})
+
+def wait(request, token):
+	return render(request, 'game/wait.html', {"token": token})
