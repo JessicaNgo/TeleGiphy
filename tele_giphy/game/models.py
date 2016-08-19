d7d046d0a58c2c76ec1fcdffdbfac5424a38ba42
Brian Wu <poenbwu@gmail.com>
2016-08-18 23:08:40 -0500

    flatten model structure

--- tele_giphy/game/models.py
+++ tele_giphy/game/models.py
@@ -1,26 +1,21 @@
 from django.db import models
 
 # Keeps tabs on a game token, whether game has started, and ended
-class GameToken(models.Model):
-    game_token = models.CharField(max_length=16)
+class Game(models.Model):
+    token = models.CharField(max_length=16)
     game_active = models.BooleanField(default='False')
     game_over = models.BooleanField(default='False')
-
-# Can be used for perma users
-class UserToken(models.Model):
-    user_token = models.CharField(max_length=16)
 
 # Keeps track of users in relation to game tokens
 class User(models.Model):
     username = models.CharField(max_length=60)
-    user_token = models.ForeignKey(UserToken, on_delete=models.CASCADE)
-    game_token = models.ForeignKey(GameToken, on_delete=models.CASCADE)
+    token = models.CharField(max_length=16)
 
 # Keeps track of game rounds
 class GameRounds(models.Model):
     round_number = models.IntegerField()
     user_text = models.CharField(max_length=150)
-    giphy_url = models.CharField(max_length=2083) #Should be AWS S3 URL, uses limitation of URL length
+    giphy_url = models.CharField(max_length=2083) #Should be the AWS S3 URL, uses limitation of URL length
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     game_token = models.ForeignKey(GameToken, on_delete=models.CASCADE)
 
