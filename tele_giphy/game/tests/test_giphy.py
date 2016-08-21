4831e650f49cf7c12023db0e35c4dab8b92569df
Brian Wu <poenbwu@gmail.com>
2016-08-20 22:00:31 -0500

    move responses decorator to function level

--- tele_giphy/game/tests/test_giphy.py
+++ tele_giphy/game/tests/test_giphy.py
@@ -2,6 +2,7 @@ import pytest
 import json
 from ..giphy import gif_translate, gif_random
 import responses
+import requests
 
 # Load JSON fixtures
 @pytest.fixture(scope='module')
@@ -10,22 +11,29 @@ def load_json(request):
     with open(load_file, 'rU') as json_file:
        return(json.load(json_file))
 
-@responses.activate
+# def setUp(self, load_json):
+#     self.json = load_json
+#     responses.add(responses.GET, 'http://api.giphy.com/v1/gifs/random', 
+#         json=json.dumps(self.json), status=200, content_type='application/json')
+
+
+
 @pytest.mark.usefixtures('load_json')
 @pytest.mark.parametrize("load_json", ['./tests/fixtures/random_001.json'], indirect=True)
 class TestRandom():
+    @responses.activate
     def test_random1(self, load_json):
-        gif_random()
+        # gif_random()
         print("Test Random 1")
         print(load_json['meta'])
         assert 0
 
-@responses.activate
+# @responses.activate
 @pytest.mark.usefixtures('load_json')
 @pytest.mark.parametrize("load_json", ['./tests/fixtures/translate_001.json'], indirect=True)
 class TestTranslate():
-    def test_random1(self, load_json):
-        gif_translate()
+    def test_translate1(self, load_json):
+        # gif_translate()
         print("Test Random 1")
         print(load_json['meta'])
         assert 0
