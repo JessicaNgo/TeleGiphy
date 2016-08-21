1c9d78a7dc64c965f9f46221d5e03207e94e918f
Brian Wu <poenbwu@gmail.com>
2016-08-20 20:40:34 -0500

    temp no request fixture

--- tele_giphy/game/tests/test_giphy.py
+++ tele_giphy/game/tests/test_giphy.py
@@ -2,21 +2,31 @@ import pytest
 import json
 from ..giphy import gif_translate, gif_random
 
-# Load fixtures
+# Load JSON fixtures
 @pytest.fixture(scope='module')
 def load_json(request):
     load_file = request.param
     with open(load_file, 'rU') as json_file:
        return(json.load(json_file))
 
-@pytest.mark.usefixtures('load_json')
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch):
+    monkeypatch.delattr("requests.sessions.Session.request")
+
+@pytest.mark.usefixtures('load_json', 'no_requests')
 @pytest.mark.parametrize("load_json", ['./tests/fixtures/random_001.json'], indirect=True)
 class TestRandom():
     def test_random1(self, load_json):
+        gif_random()
         print("Test Random 1")
         print(load_json['meta'])
         assert 0
-    def test_random2(self):
-        print("Test Random 2")
-        assert 0
 
+@pytest.mark.usefixtures('load_json', 'no_requests')
+@pytest.mark.parametrize("load_json", ['./tests/fixtures/translate_001.json'], indirect=True)
+class TestTranslate():
+    def test_random1(self, load_json):
+        gif_translate()
+        print("Test Random 1")
+        print(load_json['meta'])
+        assert 0
