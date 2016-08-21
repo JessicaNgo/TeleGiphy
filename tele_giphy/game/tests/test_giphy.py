ad60a0592670d3b9fcccdaeff90065b695c3557d
Brian Wu <poenbwu@gmail.com>
2016-08-21 18:57:17 -0500

    setup and basic format ready for tests, odd bug with connection failure

--- tele_giphy/game/tests/test_giphy.py
+++ tele_giphy/game/tests/test_giphy.py
@@ -2,7 +2,7 @@ import pytest
 import json
 from ..giphy import gif_translate, gif_random
 import responses
-import requests
+import re
 
 # Load JSON fixtures
 @pytest.fixture(scope='module')
@@ -11,29 +11,32 @@ def load_json(request):
     with open(load_file, 'rU') as json_file:
        return(json.load(json_file))
 
-# def setUp(self, load_json):
-#     self.json = load_json
-#     responses.add(responses.GET, 'http://api.giphy.com/v1/gifs/random', 
-#         json=json.dumps(self.json), status=200, content_type='application/json')
-
-
-
+# Tests giphy.gif_random
 @pytest.mark.usefixtures('load_json')
 @pytest.mark.parametrize("load_json", ['./tests/fixtures/random_001.json'], indirect=True)
 class TestRandom():
+
+    @pytest.fixture(autouse=True)
+    def setUp(self, load_json):
+        self.json = load_json
+        responses.add(responses.GET, re.compile(re.escape('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho')),
+            json=json.dumps(self.json), status=200)
+
     @responses.activate
     def test_random1(self, load_json):
-        # gif_random()
-        print("Test Random 1")
-        print(load_json['meta'])
-        assert 0
+        resp = gif_random() # == requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho')
 
-# @responses.activate
-@pytest.mark.usefixtures('load_json')
-@pytest.mark.parametrize("load_json", ['./tests/fixtures/translate_001.json'], indirect=True)
-class TestTranslate():
-    def test_translate1(self, load_json):
-        # gif_translate()
-        print("Test Random 1")
-        print(load_json['meta'])
-        assert 0
+# @pytest.mark.usefixtures('load_json')
+# @pytest.mark.parametrize("load_json", ['./tests/fixtures/translate_001.json'], indirect=True)
+# class TestTranslate():
+#     def setUp(self, load_json):
+#         self.json = load_json
+#         responses.add(responses.GET, 'http://api.giphy.com/v1/gifs/translate?s=leeroy&api_key=dc6zaTOxFJmzC',
+#             json=json.dumps(self.json), status=200, content_type='application/json')
+
+#     @responses.activate
+#     def test_translate1(self, load_json):
+#         # gif_translate()
+#         print("Test Random 1")
+#         print(load_json['meta'])
+#         assert 0
