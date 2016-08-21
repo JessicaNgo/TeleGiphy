9cc7fcfd39f62bde05cb91de272629432887ce40
Brian Wu <poenbwu@gmail.com>
2016-08-20 19:21:32 -0500

    parametrize file location

--- tele_giphy/game/tests/test_giphy.py
+++ tele_giphy/game/tests/test_giphy.py
@@ -4,15 +4,17 @@ from ..giphy import gif_translate, gif_random
 
 # Load fixtures
 @pytest.fixture(scope='module')
-def load_random_json(load_file='./tests/fixtures/random_001.json'):
+def load_json(request):
+    load_file = request.param
     with open(load_file, 'rU') as json_file:
        return(json.load(json_file))
 
-@pytest.mark.usefixtures('load_random_json')
+@pytest.mark.usefixtures('load_json')
+@pytest.mark.parametrize("load_json", ['./tests/fixtures/random_001.json'], indirect=True)
 class TestRandom():
-    def test_random1(self, load_random_json):
+    def test_random1(self, load_json):
         print("Test Random 1")
-        print(load_random_json['meta'])
+        print(load_json['meta'])
         assert 0
     def test_random2(self):
         print("Test Random 2")
