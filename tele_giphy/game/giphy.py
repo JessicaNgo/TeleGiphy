42c90cea166c53c24d53ae9beeda4b04c0963daa
Brian Wu <poenbwu@gmail.com>
2016-08-22 23:00:50 -0500

    accounting for lack of default values

--- tele_giphy/game/giphy.py
+++ tele_giphy/game/giphy.py
@@ -5,6 +5,13 @@ key = 'dc6zaTOxFJmzC'
 
 def gif_random(tag='american psycho', api_key=key):
     #Default: http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho
+    
+    # Accounting for cases where only one param is passed
+    if not tag:
+        tag = 'american psycho'
+    if not api_key:
+        api_key = key
+    
     endpoint = giphy_url + 'random'
     params = {'api_key':api_key,'tag':tag}
     data = get(endpoint, params)
@@ -12,6 +19,13 @@ def gif_random(tag='american psycho', api_key=key):
 
 def gif_translate(string='leeroy', api_key=key):
     #Default: http://api.giphy.com/v1/gifs/translate?s=leeroy&api_key=dc6zaTOxFJmzC
+    
+    # Accounting for cases where only one param is passed 
+    if not string:
+        string = 'leeroy'
+    if not api_key:
+        api_key = key
+    
     endpoint = giphy_url + 'translate'
     params = {'api_key':api_key,'s':string}
     data = get(endpoint, params)
