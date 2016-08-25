998af7375d306565cdd6c04100efb3fdf6f9edb7
Brian Wu <poenbwu@gmail.com>
2016-08-24 23:02:47 -0500

    preliminary html construction

--- tele_giphy/game/views.py
+++ tele_giphy/game/views.py
@@ -22,18 +22,21 @@ def gameover(request):
     
     # mock result for html formatting
     result = [
-        {'player': 'player1', 'rounds': 
+        {'username': 'player1', 'rounds': 
             [{'user_text': 'american psycho', 'giphy_url': 'http://media4.giphy.com/media/6nJ4uNiOwGqty/giphy.gif'},
             {'user_text': 'christian bale', 'giphy_url': 'https://media3.giphy.com/media/yaYw1xR88ccJa/giphy.gif'}, 
-            {'user_text': 'kissing', 'giphy_url': 'http://media1.giphy.com/media/3o72F3zlbWvP4kJp4c/giphy.gif'}]},
-        {'player': 'professor oak', 'rounds': 
-            [{'user_text': 'starter pokemon', 'giphy_url': 'http://media1.giphy.com/media/v6DHXicvXwJHi/giphy.gif'}
-            {'user_text': 'fire fox pokemon', 'giphy_url': 'http://media1.giphy.com/media/L2jkcvfvxkpby/giphy.gif'}
-            {'user_text': 'team ash pkmn', 'giphy_url': 'http://media4.giphy.com/media/eox3BwD5LHAQw/giphy.gif'}]},
-        {'player': 'shibe', 'rounds':
+            {'user_text': 'kissing', 'giphy_url': 'http://media1.giphy.com/media/3o72F3zlbWvP4kJp4c/giphy.gif'}]
+        },
+        {'username': 'professor oak', 'rounds': 
+            [{'user_text': 'starter pokemon', 'giphy_url': 'http://media1.giphy.com/media/v6DHXicvXwJHi/giphy.gif'},
+            {'user_text': 'fire fox pokemon', 'giphy_url': 'http://media1.giphy.com/media/L2jkcvfvxkpby/giphy.gif'},
+            {'user_text': 'team ash pkmn', 'giphy_url': 'http://media4.giphy.com/media/eox3BwD5LHAQw/giphy.gif'}]
+        },
+        {'username': 'shibe', 'rounds':
             [{'user_text': 'doge', 'giphy_url': 'https://media4.giphy.com/media/2FnQsl7O3aBC9O/giphy.gif'},
             {'user_text': 'store shibe', 'giphy_url': 'http://media0.giphy.com/media/V2zGGB4ZWE5DW/giphy.gif'},
-            {'user_text': 'weird store', 'giphy_url': 'http://media4.giphy.com/media/a0FuEjOWAxIuk/giphy.gif'}]}
+            {'user_text': 'weird store', 'giphy_url': 'http://media4.giphy.com/media/a0FuEjOWAxIuk/giphy.gif'}]
+        }
     ]
 
     return render(request, 'game/gameover.html', {"result":result})
