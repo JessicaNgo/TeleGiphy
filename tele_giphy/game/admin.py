23580cf1f891c0eadffc1868d6c34241815f837f
Brian Wu <poenbwu@gmail.com>
2016-08-27 11:20:01 -0500

    added text remaining

--- tele_giphy/game/admin.py
+++ tele_giphy/game/admin.py
@@ -4,7 +4,19 @@ from django.contrib import admin
 # Localfolder
 from .models import Game, GameRound, User
 
+# Text Form widget
+from charsleft_widget.widgets import CharsLeftInput, MediaMixin
+from django.db import models
+# The MediaMixin is what loads the css and javascript only one time per admin page
+class TextModelAdmin(MediaMixin, admin.ModelAdmin):
+  # Use widget on all instances of this form field
+  formfield_overrides = {
+    models.CharField: {'widget': CharsLeftInput()},
+  }
+
+
 # Register your models here.
-admin.site.register(Game)
-admin.site.register(User)
-admin.site.register(GameRound)
+admin.site.register(Game, TextModelAdmin)
+admin.site.register(User, TextModelAdmin)
+admin.site.register(GameRound, TextModelAdmin)
+
