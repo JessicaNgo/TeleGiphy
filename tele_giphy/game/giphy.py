0d8a830e515af2c5de9f5c05d992898d70c4cf80
Brian Wu <poenbwu@gmail.com>
2016-09-09 14:58:01 -0500

    reference API

--- tele_giphy/game/giphy.py
+++ tele_giphy/game/giphy.py
@@ -1,21 +1,21 @@
 # Third Party
 from requests import get
 
-giphy_url = 'http://api.giphy.com/v1/gifs/'
-key = 'dc6zaTOxFJmzC'
+GIPHY_URL = 'http://api.giphy.com/v1/gifs/'
+KEY = 'dc6zaTOxFJmzC'
 
 
-def gif_random(tag='american psycho', api_key=key):
-    # http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho
-    endpoint = giphy_url + 'random'
+def gif_random(tag='american psycho', api_key=KEY):
+    # See https://github.com/Giphy/GiphyAPI#random-endpoint
+    endpoint = GIPHY_URL + 'random'
     params = {'api_key': api_key, 'tag': tag}
     data = get(endpoint, params)
     return data
 
 
-def gif_translate(string='leeroy', api_key=key):
-    # http://api.giphy.com/v1/gifs/translate?s=superman&api_key=dc6zaTOxFJmzC
-    endpoint = giphy_url + 'translate'
+def gif_translate(string='leeroy', api_key=KEY):
+    # See https://github.com/Giphy/GiphyAPI#translate-endpoint
+    endpoint = GIPHY_URL + 'translate'
     params = {'api_key': api_key, 's': string}
     data = get(endpoint, params)
     return data
