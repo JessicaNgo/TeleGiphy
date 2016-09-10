601cc14762ba9ab4351fe7ef2ffc3bd1f8ae1ef4
Brian Wu <poenbwu@gmail.com>
2016-09-10 12:47:13 -0500

    start building funnel

--- tele_giphy/game/giphy.py
+++ tele_giphy/game/giphy.py
@@ -5,7 +5,7 @@ GIPHY_URL = 'http://api.giphy.com/v1/gifs/'
 KEY = 'dc6zaTOxFJmzC'
 
 
-def gif_random(tag='american psycho', api_key=KEY):
+def _gif_random(tag='american psycho', api_key=KEY):
     # See https://github.com/Giphy/GiphyAPI#random-endpoint
     endpoint = GIPHY_URL + 'random'
     params = {'api_key': api_key, 'tag': tag}
@@ -13,9 +13,17 @@ def gif_random(tag='american psycho', api_key=KEY):
     return data
 
 
-def gif_translate(string='leeroy', api_key=KEY):
+def _gif_translate(string='leeroy', api_key=KEY):
     # See https://github.com/Giphy/GiphyAPI#translate-endpoint
     endpoint = GIPHY_URL + 'translate'
     params = {'api_key': api_key, 's': string}
     data = get(endpoint, params)
     return data
+
+def giphy_call(call_type='translate', string='doge'):
+    if call_type == 'translate':
+        pass
+    elif call_type == 'random':
+        pass
+    else:
+        pass
